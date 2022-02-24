fb8f9066006ddc12c16c9f1c127a9f22d0986ac3
Laura Webber <laura@hightouch.io>
2022-02-24 13:06:19 -0800

    add new app

---
+++ react-week-3/fake-store-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(json => console.log(json))
+    
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
